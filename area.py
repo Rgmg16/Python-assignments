8e5a07283f910348b8b5111fded0bf977f2c0cbb
Rgmg16 <gumborobert7@gmail.com>
2024-05-20 20:24:21 +0300

    Assignment 1

---
+++ area.py
@@ -0,0 +1,20 @@
+# area of a square
+
+def areasq(length):
+    return length**2
+
+print(areasq(5))
+
+# area of a rectangle
+
+def areare(lenght,width):
+    return lenght * width
+
+print(areare(2,5))
+
+# area of a circle
+import math
+def areaci(radius):
+    return math.pi*radius*radius
+
+print(areaci(7))
