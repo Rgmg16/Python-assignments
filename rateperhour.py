faee4fc1aaa1bc20f79d1c2520519472c770a961
Rgmg16 <gumborobert7@gmail.com>
2024-05-22 12:41:54 +0300

    LMS assignments

---
+++ rateperhour.py
@@ -0,0 +1,5 @@
+hours= float(input("Enter hours: "))
+rate= float(input("Enter rate: "))
+pay= rate * hours
+
+print(f"Amount to pay:{pay} ")
---
+++ tempconverter.py
@@ -0,0 +1,4 @@
+celcius= float(input(f"Insert temperature in celcius: "))
+fahrenheit= float((celcius * 9/5) + 32 )
+
+print(f"The fahrenheit equivalent is {fahrenheit}")
