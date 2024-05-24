ef82e4afb24546fbb92292d218eac7c88b2ce167
Rgmg16 <gumborobert7@gmail.com>
2024-05-24 09:00:09 +0300

    Assignment 4

--- arithmetic.py
+++ arithmetic.py
@@ -1,12 +1,12 @@
 def arith(num1,operator,num2):
     if operator== '+' :
-        print(num1 + num2)
+        print(f"The result  is: {num1 + num2}")
     elif operator== '-':
-        print(num1 - num2)
+        print(f"The result is: {num1 - num2}")
     elif operator=='*':
-        print(num1 * num2)
+        print(f"The result is {num1 * num2}")
     elif operator=='/':
-        print(num1 / num2)
+        print(f"The result is: {num1 / num2} " )
     
-arith(2,'*',2)          
+arith(2,'-',2)          
                 
