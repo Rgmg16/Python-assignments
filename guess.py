090d6eb3133aeb16e5e51ab370f1847ae7ada3fd
Rgmg16 <gumborobert7@gmail.com>
2024-05-27 21:05:32 +0300

    Assignment 4

--- guess.py
+++ guess.py
@@ -1,33 +1,3 @@
-# import random
-
-# def guessing_game():
-#     # Generate a random number between 1 and 100
-#     secret_number = random.randint(1, 100)
-#     guess = None
-    
-#     print("Welcome to the guessing game!")
-#     print("I have selected a number between 1 and 100. Try to guess it!")
-
-#     # Loop until the user guesses the correct number
-#     while guess != secret_number:
-#         try:
-#             # Get the user's guess and convert it to an integer
-#             guess = int(input("Enter your guess: "))
-            
-#             if guess < 1 or guess > 100:
-#                 print("Please enter a number between 1 and 100.")
-#             elif guess < secret_number:
-#                 print("Too low! Try again.")
-#             elif guess > secret_number:
-#                 print("Too high! Try again.")
-#             else:
-#                 print(f"Congratulations! You guessed the correct number: {secret_number}")
-#         except ValueError:
-#             print("Invalid input. Please enter a valid number.")
-
-# # Run the guessing game
-# guessing_game()
-
 import random
 
 def guessing_game():
